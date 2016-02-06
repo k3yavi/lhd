355416ceb4ebc9fe054e4d5b61571d76135f52a0
keyavi <avi.srivastava@stonybrook.edu>
2016-02-06 16:48:43 -0500

    batch

--- lhd.py
+++ lhd.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from subprocess import Popen, PIPE
 import click
-from queue import Queue, Empty, Full
+from Queue import Queue, Empty, Full
 from threading import Thread, Lock
+from time import sleep
 
 #lot of refernce from http://www.troyfawkes.com/learn-python-multithreading-queues-basics/
 #verbatim from http://stackoverflow.com/questions/3185261/python-threading-and-queues-for-infinite-data-input-stream
@@ -19,19 +20,29 @@ def dump_mate(queue,
               threadId,
               mate1,
               mate2,
-              mutex):
+              mutex,
+              max_batch_size):
     while True:
         try:
-            readObject = queue.get(block=True, timeout=1)
-            midIndex = int(len(readObject.nt)/2)
-            mutex.acquire()
-            mate1.write(readObject.readName + "/2\n" + readObject.nt[:midIndex] + "\n" + '+'+readObject.readName[1:] + "/2\n" + readObject.quality[:midIndex] + "\n")
-            mate2.write(readObject.readName + "/2\n" + readObject.nt[midIndex:] + "\n" + '+'+readObject.readName[1:] + "/2\n" + readObject.quality[midIndex:] + "\n")
-            mutex.release()
-            queue.task_done()
+            m1 = ""
+            m2 = ""
+            batch_size = 0
+            while(batch_size<int(max_batch_size)):
+                batch_size += 1
+                readObject = queue.get(block=True, timeout=1)
+                midIndex = int(len(readObject.nt)/2)
+                m1 += readObject.readName + "/2\n" + readObject.nt[:midIndex] + "\n" + '+'+readObject.readName[1:] + "/2\n" + readObject.quality[:midIndex] + "\n"
+                m2 +=readObject.readName + "/2\n" + readObject.nt[midIndex:] + "\n" + '+'+readObject.readName[1:] + "/2\n" + readObject.quality[midIndex:] + "\n"
+                queue.task_done()
         except Empty:
             print ("Exiting thread {}".format(threadId))
             break
+        finally:
+            mutex.acquire()
+            mate1.write(m1)
+            mate2.write(m2)
+            mutex.release()
+
 
 
 def fill_queue(queue, filename):
@@ -49,18 +60,21 @@ def fill_queue(queue, filename):
 
 @click.command()
 @click.option('--sra', help='SRA file path. fastq will be in the same path with _1 & _2')
-def main(sra):
+@click.option('--bs', help='max batch size for writing file')
+@click.option('--p', help='threads to use')
+def main(sra, bs, p):
     readsQueue = Queue(maxsize=0)
-    noOfThreads = 2
+    noOfThreads = int(p)
     mutex = Lock()
 
     with open(sra+"_1.fq", 'w') as m1, open(sra+"_2.fq", 'w') as m2:
-        threads = [Thread(target=dump_mate, args=(readsQueue,i, m1, m2, mutex)) for i in range(noOfThreads)]
+        threads = [Thread(target=dump_mate, args=(readsQueue,i, m1, m2, mutex, bs)) for i in range(noOfThreads)]
         threads.insert(0,Thread(target=fill_queue,args=(readsQueue, sra)))
 
         for t in threads:
             t.daemon = True
             t.start()
+            sleep(3)
 
         for t in threads:
             t.join()
